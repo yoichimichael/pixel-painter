4892941be64c264dabcbbaf8517aeee8b1b7adfd
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-09-15 15:52:29 -0700

    board display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+
+
+
+*/
+
+import { useEffect, useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  app: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  cell: {
+    height: theme.spacing(7),
+    width: theme.spacing(7),
+    backgroundColor: 'red',
+    border: '1px solid black', 
+  },
+  board: {
+    
+  },
+  colorInput: {
+    border: '1px solid black'
+  }
+}))
+
+const DEFAULT_BOARD = Array(10).fill(0).map(r => Array(10).fill(0));
+
+function Cell(){
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.cell}></Box>
+  )
+}
+
+function Board(){
+  const [board, setBoard] = useState(null);
+
+  const classes = useStyles();
+  const styles = {
+    display: 'grid',
+    gridTemplateColumns: (board ? `repeat(${board[0].length}, min-content)` : 0),
+  }
+
+  useEffect(() => {
+    setBoard(DEFAULT_BOARD);
+  }, [])
+
+  return (
+    <>
+      <TextField fullwidth className={classes.colorInput}></TextField>
+      <Box className={classes.board} style={styles}>
+        {board ? board.map((row, r) => row.map((col, c) => <Cell key={`${r}-${c}`}/>)) : null}
+      </Box>
+    </>
+  )
+}
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.app}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography variant='h1' component='h1'>PIXEL PAINT</Typography>
       </header>
+      <Board/>
     </div>
   );
 }
